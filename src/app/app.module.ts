f738f97db0d4d2b62f470a2d491f8ffc6eadf094
Soliquidus <tibo.pfeifer@gmail.com>
2021-12-06 11:29:45 +0100

    DB link established, Put and Get function added for Todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
 import {TodoComponent} from './todo/todo.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -9,23 +10,10 @@ import {HomeComponent} from './home/home.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {SingleTodoComponent} from './single-todo/single-todo.component';
 import {ContactComponent} from './contact/contact.component';
-import {RouterModule, Routes} from "@angular/router";
-import { AddTodoComponent } from './todo/add-todo/add-todo.component';
-import { UsersComponent } from './users/users.component';
-import { AddUserComponent } from './users/add-user/add-user.component';
-
-export const ROUTES: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'todos', component: TodoComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'add-user', component: AddUserComponent},
-  {path: 'add-todo', component: AddTodoComponent},
-  {path: 'single-todo/:id', component: SingleTodoComponent},
-  {path: '', component: TodoComponent},
-  {path: '**', pathMatch: 'full', redirectTo: 'not-found'},
-];
+import {AddTodoComponent} from './todo/add-todo/add-todo.component';
+import {UsersComponent} from './users/users.component';
+import {AddUserComponent} from './users/add-user/add-user.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -44,7 +32,8 @@ export const ROUTES: Routes = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [TodoService],
   bootstrap: [AppComponent]
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Subject} from "rxjs";
 import {Todo} from "../models/todo.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -10,49 +11,49 @@ export class TodoService {
   todos: any = Todo;
   todosSubject = new Subject<any[]>();
 
-  // Simuler du fetching pour utiliser l'asynchrone
-  constructor() {
-    setTimeout(() => {
-      this.todos = [
-        {
-          name: "Projet 1",
-          status: true,
-          image: "https://www.placeimg.com/300/300/tech",
-          changed: false,
-          description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
-            "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
-            "Voluptatum urna convallis, animi leo, diam atque aptent vero."
-        },
-        {
-          name: "Projet 2",
-          status: false,
-          image: "https://www.placeimg.com/300/300/tech",
-          changed: false,
-          description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
-            "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
-            "Voluptatum urna convallis, animi leo, diam atque aptent vero."
-        },
-        {
-          name: "Projet 3",
-          status: true,
-          image: "https://www.placeimg.com/300/300/tech",
-          changed: false,
-          description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
-            "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
-            "Voluptatum urna convallis, animi leo, diam atque aptent vero."
-        },
-        {
-          name: "Projet 4",
-          status: false,
-          image: "https://www.placeimg.com/300/300/tech",
-          changed: false,
-          description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
-            "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
-            "Voluptatum urna convallis, animi leo, diam atque aptent vero."
-        },
-      ];
-      this.emitTodos();
-    }, 3000);
+  constructor(private httpClient: HttpClient) {
+    this.getTodosFromServer();
+    // setTimeout(() => {
+    //   this.todos = [
+    //     {
+    //       name: "Projet 1",
+    //       status: true,
+    //       image: "https://www.placeimg.com/300/300/tech",
+    //       changed: false,
+    //       description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
+    //         "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
+    //         "Voluptatum urna convallis, animi leo, diam atque aptent vero."
+    //     },
+    //     {
+    //       name: "Projet 2",
+    //       status: false,
+    //       image: "https://www.placeimg.com/300/300/tech",
+    //       changed: false,
+    //       description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
+    //         "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
+    //         "Voluptatum urna convallis, animi leo, diam atque aptent vero."
+    //     },
+    //     {
+    //       name: "Projet 3",
+    //       status: true,
+    //       image: "https://www.placeimg.com/300/300/tech",
+    //       changed: false,
+    //       description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
+    //         "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
+    //         "Voluptatum urna convallis, animi leo, diam atque aptent vero."
+    //     },
+    //     {
+    //       name: "Projet 4",
+    //       status: false,
+    //       image: "https://www.placeimg.com/300/300/tech",
+    //       changed: false,
+    //       description: "Sollicitudin eros, aptent purus suspendisse sit in omnis consectetur nostrud, placerat ornare, " +
+    //         "platea occaecati placeat, vitae pariatur facilisis ea, imperdiet phasellus? " +
+    //         "Voluptatum urna convallis, animi leo, diam atque aptent vero."
+    //     },
+    //   ];
+    //   this.emitTodos();
+    // }, 3000);
   }
 
   public emitTodos() {
@@ -62,11 +63,13 @@ export class TodoService {
   onChangeStatus(i: number) {
     this.todos[i].status = !this.todos[i].status;
     this.emitTodos();
+    this.saveTodosToServer();
   }
 
   onChangeIsModif(i: number) {
     this.todos[i].changed = !this.todos[i].changed
     this.emitTodos();
+    this.saveTodosToServer();
   }
 
   getTodo(index: number) {
@@ -79,6 +82,38 @@ export class TodoService {
   addTodo(todo: Todo): void {
     this.todos.unshift(todo);
     this.emitTodos();
+    this.saveTodosToServer();
   }
 
+  saveTodosToServer(): void {
+    this.httpClient.put("https://yourFirebaseServer/todos.json",
+      this.todos).subscribe({
+        next: () => {
+          console.log("Données enregistrées avec succès !");
+        },
+        error: (error: any) => {
+          console.log("Erreur de sauvegarde de données : " + error);
+        },
+        complete: () => {
+          console.log("Observable sur le transfert de données terminé.")
+        }
+      }
+    )
+  }
+
+  getTodosFromServer(): void {
+    this.httpClient.get<Todo[]>("https://yourFirebaseServer/todos.json")
+      .subscribe({
+        next: (todosRecup) => {
+          this.todos = todosRecup;
+          this.emitTodos()
+        },
+        error: (error) => {
+          console.log("Erreur de récupération des données : " + error)
+        },
+        complete: () => {
+          console.log("Récupération des données terminée.")
+        }
+      })
+  }
 }
